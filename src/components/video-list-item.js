ee6d27cf652132958675ea5505b79f5ede1bde1c
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-13 19:34:32 -0400

    Props et map

---
+++ src/components/video-list-item.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const VideoListItem = ({movie}) => {
+    // let movie = props.movie;
+    return <li>un film recommande : {movie}</li>
+};
+
+export default VideoListItem;
